e9d550aeb499e15d82f9467f2425ceeb0728d8ba
Daysired De Jesus <daisy@Daysireds-MBP.home>
2021-02-08 10:42:18 -0500

    working on about-me screen

---
+++ client/src/screens/About.jsx
@@ -0,0 +1,13 @@
+import './About.css';
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <div className="about-circle">
+        <h3>About Me</h3>
+      </div>
+    </div>
+  )
+}
