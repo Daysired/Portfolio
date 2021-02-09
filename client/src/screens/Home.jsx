e5f9e2e727718e1c3cfda9462a10bf98f95c8ae7
Daysired De Jesus <daisy@Daysireds-MBP.home>
2021-02-09 07:21:09 -0500

    done with home screen for now

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,11 +1,18 @@
 import './Home.css';
-
+import { Link } from 'react-router-dom';
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      {/* <img className="home-img" src='https://i.imgur.com/TrHDIEv.png' /> */}
+    <div className="container">
+      {/* <h2 className="name">Daysired De Jesus</h2>
+      <h3 className="title">Software Engineer</h3> */}
+      <img className="home-img" src='https://i.imgur.com/d2PJ7UG.png' />
+      <Link to="/about-me"><div className="indicator">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div></Link>
     </div>
   )
 }
