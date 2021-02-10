8ac91d7f6f393aa06bb9fd380fd3a40328fef11d
Daysired De Jesus <daisy@Daysireds-MBP.home>
2021-02-10 10:34:19 -0500

    done with home screen for now, will work on it's responsiveness later

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,18 +1,24 @@
-import './Home.css';
-import { Link } from 'react-router-dom';
-import React from 'react'
+import "./Home.css";
+import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Home() {
   return (
     <div className="container">
-      {/* <h2 className="name">Daysired De Jesus</h2>
-      <h3 className="title">Software Engineer</h3> */}
-      <img className="home-img" src='https://i.imgur.com/d2PJ7UG.png' />
-        <div className="indicator">
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="home-container">
+        <div className="top-text">
+          <h2 className="name">DAYSIRED DE JESUS</h2>
+          <h3 className="title">SOFTWARE ENGINEER</h3>
         </div>
+        <div className="bottom-image">
+          <img className="home-img" src="https://i.imgur.com/d2PJ7UG.png" />
+        </div>
+      </div>
+      <div className="indicator">
+        <span></span>
+        <span></span>
+        {/* <span></span> */}
+      </div>
     </div>
-  )
+  );
 }
