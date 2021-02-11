0928eda386c34210355149983cd86e8f427d1513
Daysired De Jesus <daisy@Daysireds-MBP.home>
2021-02-11 14:24:20 -0500

    fixed buttons in Everywhen screen

--- client/src/screens/Everywhen.jsx
+++ client/src/screens/Everywhen.jsx
@@ -11,14 +11,25 @@ export default function Everywhen() {
       <div className="everywhen-text-container">
         <h4 className="everywhen-title">Everywhen</h4>
         <h5 className="everywhen-details">
-            Built with react, css, and using Airtable API, Everywhen is an application that serves as a self tracker and assesment for mental health.
+          Built with react, css, and using Airtable API, 
+          Everywhen is an application that serves as a self tracker and assesment for mental health.
         </h5>
         <div className="everywhen-btn-container">
           <div className="everywhen-site">
-            <a href="https://everywhen.netlify.app/"><button>Live Site</button></a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://everywhen.netlify.app/">
+              <button clas>Live Site</button>
+            </a>
           </div>
           <div className="e-github">
-            <a href="https://github.com/Daysired"><button>Github</button></a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Daysired">
+              <button>Github</button>
+            </a>
           </div>
         </div>
       </div>
