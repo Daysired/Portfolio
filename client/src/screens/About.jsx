e1a0f6dfdb418b803f3d4c6c9815b20697111d0e
daisy <daysired345@gmail.com>
2021-12-14 12:45:56 -0500

    working on responsiveness on about me screen by using view widthunits, so that elements adapt to different screen widths

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -6,11 +6,11 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons'
 export default function About() {
   return (
   
-    <div className="image-stack">
-      <div className="about-circle-top">
+    <div className="about-container">
+      <div className="about-img-div">
         <img className="about-img" src="https://i.imgur.com/FIbZCZS.png" />
       </div>
-        <div className="about-container-bottom">
+        <div className="about-text-container">
           <h4 className="aboutscreen-title">About Me</h4>
         <h5 className="about-text">
             Hi there, I’m Daysired (aka Daisy). 
@@ -25,8 +25,8 @@ export default function About() {
           As someone who’s always worked in a fast paced environment, quick thinking
           and creative problem solving have always been my best attributes and I’m excited
           to bring these qualities and more to my next role.</h5>
-        <div className="btn-stack">
-        <div className="resume-div">
+      
+        <div className="resume-btn-div">
         <a
           target="_blank"
           rel="noreferrer"
@@ -35,8 +35,7 @@ export default function About() {
           </a>
         </div>
         
-       </div>
-
+       
        
       </div>
       </div>
